b4a39b966bf6ae28f0d80eca0d732fb17bbc226e
Speedydown <speedy@speedydown.nl>
2015-08-27 16:11:30 +0200

    v1.0

---
+++ Spreekwoorden/Spreekwoorden.Windows/MainPage.xaml.cs
@@ -0,0 +1,152 @@
+﻿using SpreekwoordenLogic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.System;
+using Windows.System.UserProfile;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using WRCHelperLibrary;
+
+namespace Spreekwoorden
+{
+    public sealed partial class MainPage : Page
+    {
+        private SpreekwoordenWrapper SpreekwoordInstance = null;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Task Loaddata = LoadData();
+        }
+
+        protected async override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            await SpreekwoordInstance.Save();
+            base.OnNavigatingFrom(e);
+        }
+
+        public async Task LoadData()
+        {
+            SpreekwoordInstance = await SpreekwoordenWrapper.GetInstance();
+            this.DataContext = SpreekwoordInstance;
+            LoadingControl.SetLoadingStatus(false);
+
+            LoadingControl.DisplayLoadingError(false);
+            LoadingControl.SetLoadingStatus(true);
+
+            await SpreekwoordInstance.GetRandomWoorden();
+
+            LoadingControl.SetLoadingStatus(false);
+
+            if (SpreekwoordInstance.ChangeLockscreen)
+            {
+                NotificationHandler.Run("SpreekwoordenBackgroundTaskW.BackgroundTask", "ImageService", 60);
+            }
+        }
+
+        private async void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                await Search();
+            }
+        }
+
+        public async Task Search()
+        {
+            LoadingControl.DisplayLoadingError(false);
+            LoadingControl.SetLoadingStatus(true);
+
+            await SpreekwoordInstance.Search(SearchTextbox.Text);
+
+            if (!SpreekwoordInstance.Searching)
+            {
+                if (SpreekwoordInstance.SearchResult.Count == 0)
+                {
+                    LoadingControl.DisplayLoadingError(true);
+                }
+
+                LoadingControl.SetLoadingStatus(false);
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Search();
+        }
+
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Spreekwoord s = e.ClickedItem as Spreekwoord;
+
+            SearchGridSwitch(s);
+        }
+
+        public void SearchGridSwitch(Spreekwoord spreekwoord)
+        {
+            if (!spreekwoord.IsInList)
+            {
+                foreach (Spreekwoord sw in SpreekwoordInstance.MyItems)
+                {
+                    if (sw.ID == spreekwoord.ID)
+                    {
+                        return;
+                    }
+                }
+
+                SpreekwoordInstance.MyItems.Add(spreekwoord);
+                spreekwoord.Notify();
+            }
+            else
+            {
+                SpreekwoordInstance.MyItems.Remove(spreekwoord);
+                spreekwoord.Notify();
+
+                foreach (Spreekwoord sw in SpreekwoordInstance.MyItems)
+                {
+                    if (sw.ID == spreekwoord.ID)
+                    {
+                        SpreekwoordInstance.MyItems.Remove(sw);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void GridviewYourItems_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            YouritemsSwitch(e.ClickedItem as Spreekwoord);
+        }
+
+        public void YouritemsSwitch(Spreekwoord spreekwoord)
+        {
+            SpreekwoordInstance.MyItems.Remove(spreekwoord);
+
+            foreach (Spreekwoord sw in SpreekwoordInstance.SearchResult)
+            {
+                if (sw.ID == spreekwoord.ID)
+                {
+                    SpreekwoordInstance.MyItems.Remove(sw);
+                    sw.Notify();
+                    return;
+                }
+            }
+
+            spreekwoord.Notify();
+        }
+    }
+}
---
+++ SpreekwoordenBackgroundTaskW/BackgroundTask.cs
@@ -0,0 +1,50 @@
+﻿using SpreekwoordenLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+using Windows.System.UserProfile;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SpreekwoordenBackgroundTaskW
+{
+    public sealed class BackgroundTask : IBackgroundTask
+    {
+        private ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+            
+            SpreekwoordenWrapper spreekwoordInstance = await SpreekwoordenWrapper.GetInstance();
+
+            List<Spreekwoord> spreekwoorden = new List<Spreekwoord>();
+
+            if (spreekwoordInstance.SourceIsRandom)
+            {
+                spreekwoorden.AddRange(await Datahandler.GetRandomSpreekwoorden(false));
+            }
+
+            if (spreekwoordInstance.SourceIsList)
+            {
+                spreekwoorden.AddRange(spreekwoordInstance.MyItems);
+            }
+
+            if (spreekwoorden.Count == 0)
+            {
+                return;
+            }
+
+            Random random = new Random();
+            Spreekwoord s = spreekwoorden[random.Next(0, spreekwoorden.Count)];
+
+            await Datahandler.GetSpreekwoordenTile(s.ID);
+            await LockScreen.SetImageFileAsync(await ApplicationData.Current.LocalFolder.GetFileAsync("Tegeltje" +  s.ID + ".jpg"));
+
+            deferral.Complete();
+        }
+    }
+}
